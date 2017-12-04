e2b3c93c0619dd7646e1835168599bf856021ccf
Sorin Burtoiu <sorin.burtoiu@gmail.com>
2017-12-04 11:11:26 +0200

    sample files

---
+++ WordGeneratorFromTemplate/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+
+namespace WordGeneratorFromTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize files and  folder to generate document
+            Initialize(out DateTime currentDate, out DirectoryInfo temporaryFolder, out FileInfo templateDocument, out FileInfo xmlDataFile);
+
+            WmlDocument openXmlPowerToolsWmlDocument = new WmlDocument(templateDocument.FullName);
+            XElement data = XElement.Load(xmlDataFile.FullName);
+            bool errors = false;
+
+            errors = Generate(currentDate, temporaryFolder, templateDocument, openXmlPowerToolsWmlDocument, data);
+        }
+
+        /// <summary>
+        /// Generate final word document starting from a Template and xml data informations.
+        /// </summary>
+        /// <param name="currentDate"></param>
+        /// <param name="temporaryFolder"></param>
+        /// <param name="templateDocument"></param>
+        /// <param name="openXmlPowerToolsWmlDocument"></param>
+        /// <param name="data"></param>
+        /// <returns>true or false</returns>
+        private static bool Generate(DateTime currentDate, DirectoryInfo temporaryFolder, FileInfo templateDocument, WmlDocument openXmlPowerToolsWmlDocument, XElement data)
+        {
+            bool errors;
+            WmlDocument wmlAssembledDoc = DocumentAssembler.AssembleDocument(openXmlPowerToolsWmlDocument, data, out errors);
+
+            if (errors)
+            {
+                Console.WriteLine("There are some errors in template " + templateDocument + " .");
+                Console.WriteLine("Watch generated document AssembledDoc.docx to see errors.");
+            }
+
+            FileInfo generatedDocument = new FileInfo(Path.Combine(temporaryFolder.FullName, string.Format("GeneratedFromTemplate{0:00}-{1:00}-{2:00}-{3:00}{4:00}{5:00}.docx", currentDate.Year, currentDate.Month, currentDate.Day, currentDate.Hour, currentDate.Minute, currentDate.Second)));
+            wmlAssembledDoc.SaveAs(generatedDocument.FullName);
+
+            return errors;
+        }
+
+        /// <summary>
+        /// Initialize files and  folder to generate document.
+        /// This can be done from app.config
+        /// </summary>
+        /// <param name="currentDate"></param>
+        /// <param name="temporaryFolder"></param>
+        /// <param name="templateDocument"></param>
+        /// <param name="xmlDataFile"></param>
+        private static void Initialize(out DateTime currentDate, out DirectoryInfo temporaryFolder, out FileInfo templateDocument, out FileInfo xmlDataFile)
+        {
+            currentDate = DateTime.Now;
+            temporaryFolder = new DirectoryInfo("Docs");
+            temporaryFolder.Create();
+
+            templateDocument = new FileInfo("Template.docx");
+            xmlDataFile = new FileInfo("XMLData.xml");
+        }
+    }
+}
---
+++ WordGeneratorFromTemplate/ReadMe.txt
@@ -0,0 +1,10 @@
+
+
+Go to Debug/Release folder and run WordContractGEneratorFromTemplate.exe file.
+
+Template.docx is the template document.
+
+XMLData is the data file.
+
+Example output files are in a 'Docs' folder
+
